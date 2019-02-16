fe555a9697532d73bc0c2a9cc043a15539c68325
chakir <yassine.chakir@ilemgroup.com>
2019-02-16 18:05:26 +0100

    initial commit

--- backend/src/main/java/fiverr/repository/UserRepository.java
+++ backend/src/main/java/fiverr/repository/UserRepository.java
@@ -11,4 +11,6 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends RevisionRepository<User, Long, Integer>, JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
 }
