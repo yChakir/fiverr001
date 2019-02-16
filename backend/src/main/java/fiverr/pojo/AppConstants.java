8424722d0aef434aab52e3895bbfc5d730de2e9d
chakir <yassine.chakir@ilemgroup.com>
2019-02-16 16:37:38 +0100

    initial commit

---
+++ backend/src/main/java/fiverr/pojo/AppConstants.java
@@ -0,0 +1,24 @@
+package fiverr.pojo;
+
+public interface AppConstants {
+
+    /**
+     * API prefix
+     */
+    String API = "api";
+
+    /**
+     * API version 1
+     */
+    String V1 = "v1";
+
+    /**
+     * Register end point path
+     */
+    String REGISTER = API + "/" + V1 + "/register";
+
+    /**
+     * Profile end point path
+     */
+    String PROFILE = API + "/" + V1 + "/profile";
+}
