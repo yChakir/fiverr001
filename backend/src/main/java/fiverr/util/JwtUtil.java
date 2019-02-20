b9671f8e5b1f882b8796e4b8a0348ec721f8cec8
chakir <yassine.chakir@ilemgroup.com>
2019-02-20 22:38:43 +0100

    fix: back-end authentication

--- backend/src/main/java/fiverr/util/JwtUtil.java
+++ backend/src/main/java/fiverr/util/JwtUtil.java
@@ -1,9 +1,11 @@
 package fiverr.util;
 
 import fiverr.entity.User;
+import fiverr.service.UserService;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
@@ -18,6 +20,9 @@ public class JwtUtil {
     public static final String TOKEN_PREFIX = "Bearer";
     public static final String HEADER_STRING = "Authorization";
 
+    @Autowired
+    private UserService userService;
+
     public String getToken(User user) {
         return Jwts.builder()
                 .setSubject(user.getUsername())
@@ -25,7 +30,7 @@ public class JwtUtil {
                 .signWith(SignatureAlgorithm.HS512, TOKEN_SECRET)
                 .claim("name", user.getName())
                 .claim("surname", user.getSurname())
-                .claim("image", user.getImage())
+                .claim("avatar", user.getAvatar())
                 .compact();
     }
 
@@ -38,7 +43,7 @@ public class JwtUtil {
 
         String username = claims.getSubject();
 
-        User user = null;
+        User user = userService.findByEmail(username);
 
         if (user == null) {
             return null;
